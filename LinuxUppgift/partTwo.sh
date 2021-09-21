8eb73c2e9c128288fa21d77ed67a193b47a7dcb3
hamed0406 <h.adeli.meadeli@gmail.com>
2021-09-21 10:22:35 +0200

    add parttwo

---
+++ LinuxUppgift/partTwo.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+sudo apt update -y  #Update all repo
+sudo apt install nginx -y #install Nginx -y with answer all question yes .
+sudo mkdir /var/www/html/hamedadeli-nginx/   #make dirctory with hamedadeli-nginx
+echo 
