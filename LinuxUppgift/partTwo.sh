53dba2926e6427a926e561c7d21236c01dcee106
hamed0406 <h.adeli.meadeli@gmail.com>
2021-09-21 10:51:20 +0200

    finla comit

--- LinuxUppgift/partTwo.sh
+++ LinuxUppgift/partTwo.sh
@@ -3,4 +3,56 @@
 sudo apt update -y  #Update all repo
 sudo apt install nginx -y #install Nginx -y with answer all question yes .
 sudo mkdir /var/www/html/hamedadeli-nginx/   #make dirctory with hamedadeli-nginx
-echo 
+
+
+echo -e  >index.html  "
+
+<html>
+
+  <head>
+
+  </head>
+
+  <body>
+
+  <h1>Hamed Adeli</h1>
+
+  </body>
+
+</html>"
+
+  # create  index.html 
+
+sudo touch /etc/nginx/sites-enabled/hamedadeli
+
+echo -e  > "server {
+
+       listen 8080;
+
+       listen [::]:8080;
+
+
+
+       server_name hamedadeli.com;
+
+
+
+       root /var/www/html/hamedadeli-nginx;
+
+       index index.html;
+
+
+
+       location / {
+
+               try_files $uri $uri/ =404;
+
+       }
+
+}
+
+# create  config  for  static website 
+
+sudo systemctl restart nginx.service  # retart nginx
+sudo systemctl enable nginx.service   #enbale enginx 
+curl  localhost:8080
